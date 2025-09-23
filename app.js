ec9e89820dccb61348ab5d0eb7f454ab8f3952eb
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2025-09-24 02:17:21 +0600

    signup api

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Import the auth routes
+const authRoutes = require('./routes/auth');
+
+app.use('/auth', authRoutes);
 
 
 app.listen(port, () => {
