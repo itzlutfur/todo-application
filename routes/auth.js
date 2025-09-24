c9bbaa64ffe216aeb47084967c1480daf68067e1
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2025-09-24 16:43:16 +0600

    signup api done

--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const bcrypt = require('bcrypt');
-const path = require('path');
-const { hash } = require('crypto');
+const fs = require("fs");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const { hash } = require("crypto");
 
-const usersFilePath = path.join(__dirname, '../users.json');
+const usersFilePath = path.join(__dirname, "../users.json");
 
-router.post('/signup', (req, res) => {
-    const {id,username,password} = req.body;
-    let data = [];
+router.post("/signup", (req, res) => {
+  const { id, username, password } = req.body;
+  let data = [];
 
-    bcrypt.hash(password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        } else {
-            const userData = { id, username, password:hash };
-            const raw = fs.readFileSync(usersFilePath);
-            data = JSON.parse(raw);
-            data.push(userData);
-            fs.writeFileSync(usersFilePath, JSON.stringify(data,null,2));
-        }
-    });  
-    res.status(200).json({id,username,password:hash});
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    } else {
+      const raw = fs.readFileSync(usersFilePath);
+      data = JSON.parse(raw);
+      if (data.find((user) => user.username === username || user.id === id)) {
+        return res.status(400).json({ error: "User already exists" });
+      } else {
+        const userData = { id, username, password: hash };
+        data.push(userData);
+        fs.writeFileSync(usersFilePath, JSON.stringify(data, null, 2));
+        res.status(200).json(userData);
+      }
+    }
+  });
 });
 
-
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // Handle user login logic here
-  res.send('User logged in successfully!');
-
+  res.send("User logged in successfully!");
 });
 
 module.exports = router;
